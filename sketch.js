764a58963032ca7be4fce74c4536e095b289ce57
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-05-01 02:15:31 -0300

    Movimentação do personagem, física e construção de mapa com tiles

--- sketch.js
+++ sketch.js
@@ -1,10 +1,58 @@
 let img;
+let player;
+let walls;
 
-function setup() {
-  createCanvas(500, 500);
+function preload() {
   img = loadImage('assets/me.jpg');
 }
 
+function setup() {
+  //createCanvas(500, 500);
+  createCanvas(1000, 500);
+  game()
+}
+
 function draw() {
-  Telas();
+  //Telas();
+  clear();
+  background("#6d8b89");
+
+  //camera
+  //camera.zoom = 2;
+  //camera.x = player.x;
+  //camera.y = player.y;
+
+  // moving keys
+  if(kb.pressing('a')){
+    player.x -= 5
+  } else if (kb.pressing('d')) {
+    player.x += 5
+  }
+  
+  if (kb.pressing('space') && player.colliding(walls) ) {
+    player.vel.y = -5
+  }
+}
+
+function game() {
+  world.gravity.y = 10;
+  player = new Sprite(100, 100, 32, 33);
+  player.color = "red";
+
+  //mapa
+  walls = new Group()
+  walls.w = 50
+  walls.h = 50
+  walls.tile = "="
+  walls.collider = 'static'
+
+  new Tiles(
+    [  
+      '======================'
+    ],
+    0,
+    500,
+    walls.w,
+    walls.h
+  )
 }
