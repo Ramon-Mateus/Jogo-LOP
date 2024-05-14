21fb1d7f185cee6ec41e842f8d717686a3971db8
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-05-14 15:23:58 -0300

    feat: Second Level, camera zoom, and block opacity

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+// variáveis
 let img;
 let player;
 let walls;
@@ -5,16 +6,21 @@ let playerAni;
 let playerRun;
 let bg;
 let grassSheet;
+//let bgSong;
+let tileMap;
+let placaSheet;
 
 // Constantes
-const playerScale = 2
+const playerScale = 1
 
 function preload() {
   img = loadImage('assets/me.jpg');
+  //bgSong = loadSound('assets/bg.wav');
   playerIdle = loadAnimation('idle', 'assets/character/idle.png', { frameSize: [64, 64], frames: 4 });
   playerRun = loadAnimation('run', 'assets/character/run.png', { frameSize: [80, 80], frames: 8 }); // PORQUE BUGA COM 64 X 64 ?????????
   bg = loadImage('assets/background.png')
   grassSheet = loadImage('assets/grama.png')
+  placaSheet = loadImage('assets/placa.png')
   playerJump = loadAnimation('jump', 'assets/character/jump.png', { frameSize: [64, 64], frames: 15 });
   playerJump.frameDelay = 4;
 }
@@ -22,7 +28,7 @@ function preload() {
 function setup() {
   //createCanvas(500, 500);
   createCanvas(windowWidth/1.4, windowHeight/1.4);
-  game()
+  FirstLevel()
 }
 
 function draw() {
@@ -31,9 +37,9 @@ function draw() {
   background(bg);
 
   //camera
-  //camera.zoom = 2;
-  //camera.x = player.x;
-  //camera.y = player.y;
+  camera.zoom = 2;
+  camera.x = player.x;
+  camera.y = player.y;
 
   // moving keys
   if(kb.pressing('a')){
@@ -53,7 +59,6 @@ function draw() {
 
   } else if(player.colliding(walls) || player.colliding(grass)) {
     player.vel.y = 0
-    console.log("colidindo")
   } else {
     player.changeAni('jump');
   }
@@ -62,25 +67,25 @@ function draw() {
     player.x = 50;
     player.y = 500;
   }
+
+  if(player.colliding(tl)) {
+    LevelTwo();
+  }
 }
 
-function game() {
+function FirstLevel() {
   allSprites.pixelPerfect = true;
+  //bgSong.loop();
   world.gravity.y = 10;
-  player = new Sprite(50, 500);
-  player.addAni(playerIdle);
-  player.addAni(playerRun);
-  player.addAni(playerJump);
-  player.scale = playerScale
-  player.rotationLock = true;
-  player.removeColliders();
-  //player.debug = true;
-  player.addCollider(0, 0, 40, 125)
 
   //mapa
   walls = new Group();
   walls.w = 50;
   walls.h = 50;
+  walls.color = color(0, 0, 0, 0);
+  walls.draw = function() {
+    noStroke();
+  }
   walls.tile = "=";
   walls.collider = 'static';
 
@@ -90,31 +95,89 @@ function game() {
   grass.addAni({ w:50, h:50});
   grass.tile = "g";
 
+  placa = new Group();
+  placa.collider = 'none';
+  placa.spriteSheet = placaSheet;
+  placa.addAni({ w:512, h:512});
+  placa.scale = 0.1
+  placa.tile = "p";
+
+
   end = new Group();
   end.collider = 'static'
+  end.color = color(0, 0, 0, 0);
+  end.draw = function() {
+    noStroke();
+  }
   end.w = 50;
   end.h = 50
   end.tile = "%"
 
-  new Tiles(
+  tl = new Group();
+  tl.collider = 'static'
+  tl.color = color(0, 0, 0, 0);
+  tl.draw = function() {
+    noStroke();
+  }
+  tl.w = 50;
+  tl.h = 50
+  tl.tile = "#"
+
+  tileMap = new Tiles(
     [  
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'g............................g',
-      'gggggggggggg.....ggggggggggggg',
-      'g............................g',
-      'g............................g',
-      'g............................g',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.....................................................p.#',
+      '=ggggggggggg.....gggggggggggggggggggggggggggggggggggggggggggggggggg',
+      '=.......................................................#',
+      '=.......................................................#',
+      '=.......................................................#',
+      '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+    ],
+    -30,
+    0,
+    end.w,
+    end.h
+  )
+
+  playerSet();
+}
+
+function LevelTwo() {
+  clear();
+  player.remove();
+  playerSet();
+  
+  tileMap.remove();
+  tileMap = new Tiles(
+    [  
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
+      'g...........gggg.............#',
+      'ggggggggggggggggggggggggggggg#',
+      'g............................#',
+      'g............................#',
+      'g............................#',
       '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
     ],
     -30,
@@ -122,4 +185,16 @@ function game() {
     end.w,
     end.h
   )
+}
+
+function playerSet() {
+  player = new Sprite(50, 500);
+  player.addAni(playerIdle);
+  player.addAni(playerRun);
+  player.addAni(playerJump);
+  player.scale = playerScale
+  player.rotationLock = true;
+  player.removeColliders();
+  //player.debug = true;
+  player.addCollider(0, 0, 30, 60)
 }
