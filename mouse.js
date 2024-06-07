9136bd38da70df68163a0cbce64492c69f3f1b44
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-06-06 22:16:27 -0300

    atualizado responsividade do menu

--- mouse.js
+++ mouse.js
@@ -1,14 +1,14 @@
 function mouseClicked() {
-    if(mouseX >= 175 && mouseX <= 325 && mouseY >= 185 && mouseY <= 232) {
+    if(mouseX >= width/2 - 125 && mouseX <= width/2 + 25 && mouseY >= 185 && mouseY <= 232) {
       if(TELA === MENU) {
         TELA = GAME;
         setup()
       }
-    } else if (mouseX >= 175 && mouseX <= 325 && mouseY >= 247 && mouseY <= 292) {
+    } else if (mouseX >= width/2 - 125 && mouseX <= width/2 + 25 && mouseY >= 247 && mouseY <= 292) {
       if(TELA === MENU) {
         TELA = CONTROLS;
       }
-    } else if (mouseX >= 175 && mouseX <= 325 && mouseY >= 307 && mouseY <= 351) {
+    } else if (mouseX >= width/2 - 125 && mouseX <= width/2 + 25 && mouseY >= 307 && mouseY <= 351) {
       if(TELA === MENU) {
         TELA = CREDITS;
       }
--- telas.js
+++ telas.js
@@ -9,35 +9,32 @@ function Telas() {
   rectMode(CENTER);
   textAlign(CENTER, CENTER);
   if(TELA === GAME) {
-    fill("#d5e0b5")
-    text('Em construção!', 250, 250);
-    rect(50, 50, 30, 30, 5);
-    fill("#47667b")
-    text("<", 50, 50)
+    text("<", 50, 50) // COLOCAR ISSO NA TELA DO GAME
   } else if (TELA === CREDITS) {
     fill("#d5e0b5")
     rect(50, 50, 30, 30, 5);
-    text("Desenvolvido por: \nRamon de Oliveira Araújo Mateus\nramon.mateus.017@ufrn.edu.br", 250, 250);
-    text("Ideia: Jogo de plataforma", 250, 350);
+    text("Desenvolvido por: \nRamon de Oliveira Araújo Mateus\nramon.mateus.017@ufrn.edu.br", width/2 - 50, height/2 - 50);
+    text("Ideia: Jogo de plataforma", width/2 - 50, height + 50);
     fill("#47667b")
     text("<", 50, 50)
-    image(img, width/2-60, 85, 120, 120);
+    image(img, width/2 - 110, 85, 120, 120);
   } else if (TELA === CONTROLS) {
     fill("#d5e0b5")
     rect(50, 50, 30, 30, 5);
-    text("Irá mostrar como os controles funcionam", 250, 250);
+    text("Irá mostrar como os controles funcionam", width/2 - 50, height/2 - 50);
     fill("#47667b")
     text("<", 50, 50)
   } else {
     fill("#d5e0b5")
-    text("Nome do jogo", 250, 100)
-    rect(250, 210, 150, 45, 15);
-    rect(250, 270, 150, 45, 15);
-    rect(250, 330, 150, 45, 15);
+    text("Nome do jogo", width/2 - 50, 100)
+    rect(width/2 - 50, 210, 150, 45, 15);
+    rect(width/2 - 50, 270, 150, 45, 15);
+    rect(width/2 - 50, 330, 150, 45, 15);
     fill("#47667b")
     textSize(22)
-    text("Jogar", 250, 210)
-    text("Controles", 250, 270)
-    text("Créditos", 250, 330)
+    text("Jogar", width/2 - 50, 210)
+    text("Controles", width/2 - 50, 270)
+    text("Créditos", width/2 - 50, 330)
+    console.log(width/2 - 50)
   }
 }
